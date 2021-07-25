0ffc569b0a7fa5a3abf5ba2bbf2b459d2a3dfbac
Katsuragawa <andre.katsuragawa@gmail.com>
2021-07-25 00:25:18 -0300

    create app

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,43 @@
-const express = require('express');
+const express = require('express'); // import express - CommonJS format
+require('express-async-errors');
 const cors = require('cors');
-require('dotenv').config();
+const mongoose = require('mongoose');
+// const partnersRouter = require('./controllers/partners');
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
 
-const app = express();
+const app = express(); // create an express application
+
+// connect to database
+logger.info('Connecting to', config.MONGODB_URI);
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info('Connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB', error.message);
+  });
+
 app.use(cors());
+app.use(express.static('build')); // serve the static file (frontend in this case)
+app.use(express.json()); // parse incoming request with json payloads
 
+app.use(middleware.requestLogger);
+
+// app.use('/api/partners', partnersRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
